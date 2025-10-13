51ed3a7bdf71d8e62d4bc504b4ed2a0449afdac1
Aya Abudabbour <ayaabudabbour@gmail.com>
2025-10-13 03:22:35 +0300

    Add placeholder views for home, services, booking

--- studio_app/views.py
+++ studio_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Home page view
+def home_view(request):
+    return render(request, 'home.html')
+
+# Services page view
+def services_view(request):
+    return render(request, 'services.html')
+
+# Booking creation page view
+def booking_create(request):
+    return render(request, 'booking/booking.html')
+
+# Booking history page view
+def booking_list(request):
+    return render(request, 'booking/booking_history.html')
