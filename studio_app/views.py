6d1ba21f8bd6af6935d5431ae5fc61c9ab616de2
Aya Abudabbour <ayaabudabbour@gmail.com>
2025-10-13 15:58:58 +0300

    Redirect signup to login and display username after login

--- studio_app/views.py
+++ studio_app/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import FormView #???
 from django.views.generic.edit import CreateView
-from .forms import CustomUserCreationForm #?????
+from .forms import CustomUserCreationForm 
+from django.contrib.auth.decorators import login_required
 
+
+#FBV
 # Home page view
 def home_view(request):
     return render(request, 'home.html')
@@ -14,19 +17,23 @@ def services_view(request):
     return render(request, 'services.html')
 
 # Booking creation page view
+@login_required
 def booking_create(request):
     return render(request, 'booking/booking.html')
 
 # Booking history page view
+@login_required
 def booking_list(request):
     return render(request, 'booking/booking_history.html')#not sure about this 
 
 # User profile page view
+@login_required
 def profile_view(request):
     return render(request, 'profile.html')
 
 
+# CBV for user signup
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm  
     template_name = "registration/signup.html"
-    success_url = reverse_lazy("home") 
+    success_url = reverse_lazy("log in") 
